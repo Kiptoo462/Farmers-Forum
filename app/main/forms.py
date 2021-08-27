cc02f1f37d0f68c4d3ad5183fc041b18d391191d
Kiptoo Victor <victor.kiptoo@student.moringaschool.com>
2021-08-27 13:45:51 +0300

    Add main folder

---
+++ app/main/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField, TextAreaField,
+                    SubmitField, SelectField)
+from wtforms.validators import Required
+
+class PostForm(FlaskForm):
+    title = StringField("New post:", validators=[Required()])
+    post = TextAreaField("Type Away:", validators=[Required()])
+    submit = SubmitField("Post")
+
+class UpdatePostForm(FlaskForm):
+    title = StringField("Update post", validators=[Required()])
+    post = TextAreaField("Type Away", validators=[Required()])
+    submit = SubmitField("Update ")
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField("Post Comment", validators=[Required()])
+    alias = StringField("Comment Alias")
+    submit = SubmitField("Comment")
+
+class UpdateProfile(FlaskForm):
+    first_name = StringField("First name")
+    last_name = StringField("Last Name")
+    bio = TextAreaField("Bio")
+    email = StringField("Email")
+    submit = SubmitField("Update")
